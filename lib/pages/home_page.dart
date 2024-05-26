e32cf0cf8ae6c732468d2ca1e4c3e288bcac1b29
taruunnnnn <tarunsri0017@gmail.com>
2024-05-26 22:04:36 +0530

    Development Phase, 6%

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,8 +4,10 @@ import 'package:portfolio/components/round_cursor.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
 import '../widgets/greeting_changer.dart';
 
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required String title});
+  const HomePage({super.key, required String title,});
+
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,50 +16,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final AutoScrollController _scrollController = AutoScrollController();
 
+
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
+
     return CustomCursorWrapper(
       child: Scaffold(
         backgroundColor: Colors.black,
+
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text("Tarun Srivastava", style: GoogleFonts.ibmPlexMono(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w500),),
+            ],
+          ),
+        ),
 
         body:  ListView(
             children: [
 
               Padding(
-                padding: const EdgeInsets.only(left: 300.0, top: 60.0),
-                child: GreetingChanger(),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 300.0,),
-                child: Text(
-                  "i'm",
-                  style: GoogleFonts.ibmPlexMono(fontSize: 50, color: Colors.white, fontWeight: FontWeight.w500),
+                padding: EdgeInsets.only(right: screenWidth * 0.2,left: screenWidth * 0.2,top: screenHeight * 0.10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GreetingChanger(),
+                    Text(
+                      "i'm",
+                      style: GoogleFonts.ibmPlexMono(fontSize: 50, color: Colors.white, fontWeight: FontWeight.w500),
 
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 300.0,),
-                child: Text(
-                  "tarun.",
-                  style: GoogleFonts.ibmPlexMono(fontSize: 50, color: Colors.white, fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "tarun.",
+                      style: GoogleFonts.ibmPlexMono(fontSize: 50, color: Colors.white, fontWeight: FontWeight.w500),
 
-                ),
-              ),
+                    ),
 
-              Container(
-                padding: const EdgeInsets.only(left: 300.0, top: 10, right: 400),
-                child: Text(
-                  "I'm a software developer dedicated to crafting impactful solutions that elegantly address users' needs. With a focus on simplicity and efficiency, I integrate elements of personal life into my designs, prioritizing user experience and rights to create meaningful interactions. My work captures the essence of life by highlighting what's essential, resonating with users on both practical and emotional levels.",
-                  style: GoogleFonts.ibmPlexMono(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w400),
-                
-                ),
-              ),
+                    Container(
+                      child: Text(
+                        "I'm a software developer dedicated to crafting impactful solutions that elegantly address users' needs. With a focus on simplicity and efficiency, I integrate elements of personal life into my designs, prioritizing user experience and rights to create meaningful interactions. My work captures the essence of life by highlighting what's essential, resonating with users on both practical and emotional levels.",
+                        style: GoogleFonts.ibmPlexMono(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w400),
 
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 300.0, top: 40,),
-                  child: OutlinedButton(
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 40,
+                    ),
+
+                    OutlinedButton(
                       onPressed: () {
                         print('Button tapped');
                       },
@@ -70,15 +82,23 @@ class _HomePageState extends State<HomePage> {
                       ),
 
                       child: Text(
-                        'Say hi.',
-                        style: GoogleFonts.ibmPlexMono(
-                          color: Colors.white, // Text color
-                          fontSize: 16,
-                        )
+                          'Say hi.',
+                          style: GoogleFonts.ibmPlexMono(
+                            color: Colors.white, // Text color
+                            fontSize: 16,
+                          )
                       ),
                     ),
+                  ],
                 ),
               ),
+
+              SizedBox(
+                height: 50,
+              ),
+
+
+
 
 
 
